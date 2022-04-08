a6a1d59ea0f5662a94849a486e9ee32d68cdc8b0
Batdorj <batdorj.ds@gmail.com>
2022-04-08 11:20:00 +0800

    implemented

--- map.js
+++ map.js
@@ -1,16 +1,20 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const ulanbator = { lat: 47.915972, lng: 106.914898 };
+    const ulanbator = { lat: 47.9164365, lng: 106.914070 };
     // The map, centered at ulanbator
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
+      zoom: 17,
       center: ulanbator,
     });
     // The marker, positioned at ulanbator
     const marker = new google.maps.Marker({
       position: ulanbator,
       map: map,
+      icon:'/assets/images/company/map_group2.png',
+      // adding custom icons (Here I used a Flash logo marker)
+      draggarble: false// If set to true you can drag the marker
     });
+    // const img =  "/assets/images/company/map_stick_grp.png"
   }
   
