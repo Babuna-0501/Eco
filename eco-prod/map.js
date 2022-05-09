53648136abb2a5e8a331d4ba44c82c87f985fd9d
Batdorj <batdorj.ds@gmail.com>
2022-05-09 16:53:43 +0800

    fixed after review

--- eco-prod/map.js
+++ eco-prod/map.js
@@ -1,17 +1,18 @@
 function initMap() {
     // The location of Uluru
-    const ulanbator = { lat: 47.9164365, lng: 106.914070 };
+    const ulanbator = { lat: 47.9164365, lng: 106.914520 };
     // The map, centered at ulanbator
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 17,
+      zoom: 16,
       center: ulanbator,
     });
     // The marker, positioned at ulanbator
-    const marker = new google.maps.Marker({
-      position: ulanbator,
-      map: map,
-      icon:'/assets/images/company/map_group2.png',
-      draggarble: false
+    const image =
+    "/assets/marker_map.svg";
+    const beachMarker = new google.maps.Marker({
+      position: { lat: 47.9164365, lng: 106.914520 },
+      map,
+      icon: image,
     });
   }
   
